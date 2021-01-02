048991ce3d6fb5523bc0bac7229e43d09ad4e34d
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-02 18:55:56 -0300

    TaskList.js screen part 4 - remove font

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 
+import commonStyles from '../commomStyles'
 import todayImage from '../../assets/imgs/today.jpg'
 
 import moment from 'moment'
@@ -14,8 +15,8 @@ export default class TaskList extends Component {
          <View style = {styles.container}>
             <ImageBackground source = {todayImage} style = {styles.background}>
                <View style = {styles.titlebar}>
-                  <Text>Hoje</Text>
-                  <Text>{today}</Text>
+                  <Text style = {styles.title}>Hoje</Text>
+                  <Text style = {styles.subTitle}>{today}</Text>
                </View>
             </ImageBackground>
 
@@ -40,5 +41,19 @@ const styles = StyleSheet.create({
    titlebar: {
       flex: 1,
       justifyContent: 'flex-end'
+   },
+   title: {
+      // fontFamily: "Lato",
+      color: commonStyles.colors.secondary,
+      fontSize: 50,
+      marginLeft: 20,
+      marginBottom: 20
+   },
+   subTitle: {
+       // fontFamily: "Lato",
+       color: commonStyles.colors.secondary,
+       fontSize: 20,
+       marginLeft: 20,
+       marginBottom: 30
    }
 })
