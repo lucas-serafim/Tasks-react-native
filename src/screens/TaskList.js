8a1156f1bdf98689d56b42fb6f78c4b969ef5737
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-02 17:05:18 -0300

    TaskList.js screen part 3 - get moment

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -3,13 +3,22 @@ import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 
 import todayImage from '../../assets/imgs/today.jpg'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 export default class TaskList extends Component {
    render() {
+      const today = moment().local('pt-br').format('ddd, D [de] MMMM')
+
       return (
          <View style = {styles.container}>
             <ImageBackground source = {todayImage} style = {styles.background}>
-
+               <View style = {styles.titlebar}>
+                  <Text>Hoje</Text>
+                  <Text>{today}</Text>
+               </View>
             </ImageBackground>
+
             <View style = {styles.taskList}>
                <Text>TaskList</Text>
             </View>  
@@ -27,5 +36,9 @@ const styles = StyleSheet.create({
    },
    taskList: {
       flex: 7
+   },
+   titlebar: {
+      flex: 1,
+      justifyContent: 'flex-end'
    }
 })
