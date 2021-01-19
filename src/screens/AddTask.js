b9eccf9fa6c3814174b0a101bd1c11fb272fd99f
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-19 16:00:15 -0300

    AddTasks modal part 2

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
+import commomStyles from '../commomStyles'
 
 export default class AddTask extends Component {
 
    render() {
       return (
          <Modal transparent = {true} visible = {this.props.isVisible} onRequestClose = {this.props.onCancel} animationType = "slide">
+            <TouchableWithoutFeedback onPress = {this.props.onCancel}>
+               <View style = {styles.background}></View>
+            </TouchableWithoutFeedback>
+
+            <View style = {styles.container}>
+               <Text style = {styles.header}>Nova Tarefa</Text>
+            </View>
+
             <TouchableWithoutFeedback onPress = {this.props.onCancel}>
                <View style = {styles.background}></View>
             </TouchableWithoutFeedback>
@@ -18,5 +27,16 @@ const styles = StyleSheet.create({
    background: {
       flex: 1,
       backgroundColor: "rgba(0, 0, 0, 0.7)"
+   }, container: {
+      flex: 1,
+      backgroundColor: '#FFF'
+   },
+   header: {
+      //fontFamily:
+      backgroundColor: commomStyles.colors.today,
+      color: commomStyles.colors.secondary,
+      textAlign: 'center',
+      padding: 15,
+      fontSize: 18
    }
 })
