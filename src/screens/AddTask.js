50de489687f0174b56808de27f4ba09b64e40b4f
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-20 15:44:30 -0300

    Task date part 2

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text, TextInput, TouchableOpacity } from 'react-native'
+import { Platform, Modal, View, StyleSheet, TouchableWithoutFeedback, Text, TextInput, TouchableOpacity } from 'react-native'
 
 import DateTimePicker from '@react-native-community/datetimepicker'
+import moment from 'moment'
 
 import commomStyles from '../commomStyles'
 
-const initialState = { desc: '', date: new Date() }
+const initialState = { desc: '', date: new Date(), showDatePicker: false }
 
 export default class AddTask extends Component {
 
@@ -13,11 +14,28 @@ export default class AddTask extends Component {
       ...initialState
    }
 
-   getDateTimePicker = () => {
-      return <DateTimePicker 
+   getDatePicker = () => {
+      let datePicker = <DateTimePicker 
          value = {this.state.date}
-         onChange = {(_, date) => this.setState({ date })}
+         onChange = {(_, date) => this.setState({ date, showDatePicker: false })}
          mode = 'date' />
+
+      const dateString = moment(this.state.date).format('ddd, D [de] MMMM [de] YYYY')
+
+      if(Platform.OS === 'android') {
+         datePicker = (
+            <View>
+               <TouchableOpacity onPress = {() => this.setState({ showDatePicker: true })}>
+                  <Text style = {styles.date}>
+                     {dateString}
+                  </Text>
+               </TouchableOpacity>
+               { this.state.showDatePicker && datePicker }
+            </View>
+         )
+      }
+      
+      return datePicker
    }
 
    render() {
@@ -30,7 +48,7 @@ export default class AddTask extends Component {
             <View style = {styles.container}>
                <Text style = {styles.header}>Nova Tarefa</Text>
                <TextInput style = {styles.input} placeholder = "Informe a Descrição" onChangeText = {desc => this.setState({ desc })} value = {this.state.desc} />
-               {this.getDateTimePicker()}
+               {this.getDatePicker()}
                <View style = {styles.buttons}>
                   <TouchableOpacity onPress = {this.props.onCancel}>
                      <Text style = {styles.button}>Cancelar</Text>
@@ -82,5 +100,10 @@ const styles = StyleSheet.create({
       margin: 20,
       marginRight: 30,
       color: commomStyles.colors.today
+   },
+   date: {
+      //fontFamily: commomStyles.fontFamily,
+      fontSize: 20,
+      marginLeft: 15
    }
 })
