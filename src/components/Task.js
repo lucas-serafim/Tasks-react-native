19b2aeb7c9ebf4f18f0209ae5dffbd6effd53559
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-21 17:34:28 -0300

    Delete task part 3

--- src/components/Task.js
+++ src/components/Task.js
@@ -19,7 +19,7 @@ export default props => {
 
    const getRightContent = () => {
       return (
-         <TouchableOpacity style = {styles.right}>
+         <TouchableOpacity style = {styles.right} onPress = {() => props.onDelete && props.onDelete(props.id)}>
             <Icon name = "trash" size = {30} color = "#FFF" />
          </TouchableOpacity>
       )
@@ -35,9 +35,9 @@ export default props => {
    }
 
    return (
-      <Swipeable renderRightActions = {getRightContent} renderLeftActions = {getLeftContent}>
+      <Swipeable renderRightActions = {getRightContent} renderLeftActions = {getLeftContent} onSwipeableLeftOpen = {() => props.onDelete && props.onDelete(props.id)}>
          <View style = {styles.container}>
-            <TouchableWithoutFeedback onPress = {() => props.toggleTask(props.id)}>
+            <TouchableWithoutFeedback onPress = {() => props.onToggleTask(props.id)}>
                <View style = {styles.checkContainer}>
                   {getCheckView(props.doneAt)}
                </View>
