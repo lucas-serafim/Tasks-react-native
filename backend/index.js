2ea237322120361beb2ef4e838c87b3abf0d12b3
lucas-serafim <lucas.serafim159@gmail.com>
2021-02-02 12:35:36 -0300

    Middlewares & DB with Knex part 1

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (request, response) => {
-   response.status(200).send('Backend!')
-})
-
 app.listen(3000, () => {
    console.log('Backend is working!')
 })
